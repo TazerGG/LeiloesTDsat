9f2fc93ee07ff7590e2b2f6672c0b6dbf349a774
= <=>
2026-06-30 15:50:05 -0300

    Botão Cadastrar Produto salvando no Banco de Dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -23,11 +24,21 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        try {
+            String sql = "INSERT INTO PRODUTOS (nome, valor, status) VALUES (?, ?, ?)";
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, "A Venda");
+            
+            resultset = prep.executeQuery();
+            
+            prep.close();
+        } catch (SQLException e) {
+            
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
